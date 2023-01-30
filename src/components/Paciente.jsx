e115d11135acd6edd340acb10491c81ff3877248
Máximo Gómez <ardanyyy@gmail.com>
2023-01-29 21:19:54 -0600

    configuracion talwind css y menejo de hooks

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,42 @@
+
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shado-md px-5 py-10 rounded-xl">
+             <p className="font-bold mb-3 text-gray-700 uppercase">
+          Nombre: {""}
+          <span className="font-normal normal-case">Neron</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Propietario: {""}
+          <span className="font-normal normal-case">Maximo</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Email: {""}
+          <span className="font-normal normal-case">ardanyyy502@gmail.com</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Fecha Alta: {""}
+          <span className="font-normal normal-case">
+            10 de Diciembre del 2022
+          </span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Sintomas: {""}
+          <span className="font-normal normal-case">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Perspiciatis labore laborum aliquam laboriosam a dolores nobis, ipsa
+            delectus iste doloribus veniam ipsum repudiandae itaque magnam,
+            praesentium exercitationem unde. Nam, natus.
+          </span>
+        </p>
+    </div>
+  )
+}
+
+
+
+export default Paciente

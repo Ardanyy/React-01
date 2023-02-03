501aadf0147ca6afb0303b443414f93dca7124a4
Máximo Gómez <ardanyyy@gmail.com>
2023-02-02 21:47:46 -0600

    manejo de localstorage y de varios useEffect al mismo tiempo y mandar una funcion del padre a hijo componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
-import { useState} from "react";
+import { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 function App() {
+  const localPaciente = JSON.parse(localStorage.getItem("pacientes")) ?? []
   const [pacientes, setPacientes] = useState([]);
-  const [paciente, setPaciente]= useState({});
+  const [paciente, setPaciente] = useState({});
+
+  //solo una vez se ejecuta cuando viene vacio pueden agregar varios efects y como se agregan asi aparece el orden
+  useEffect(() => {
+    console.log("1")
+    const obtenerLS = () => {
+          setPacientes(localPaciente)
+    };
+    obtenerLS();
+  }, []);
+
+  useEffect(() => {
+    console.log("2")
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]);
+  const eliminarPaciente = (id) => {
+    const pacientesActualizados = pacientes.filter(
+      (paciente) => paciente.id !== id
+    );
+    setPacientes(pacientesActualizados);
+  };
 
   return (
     <div className="container mx-auto mt-20">
-      <Header 
-      />
+      <Header />
       <div className="mt-12 md:flex">
         <Formulario
-          pacientes={pacientes} 
+          pacientes={pacientes}
           setPacientes={setPacientes}
-          paciente = {paciente} 
-          setPaciente = {setPaciente}
+          paciente={paciente}
+          setPaciente={setPaciente}
         />
-        <ListadoPacientes pacientes={pacientes} setPaciente={setPaciente}/>
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
       </div>
     </div>
   );
